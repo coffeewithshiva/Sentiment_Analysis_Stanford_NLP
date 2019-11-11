951c555affabdb1c82467fa7eef2d8adf8259fb0
coffeewithshiva <57612110+coffeewithshiva@users.noreply.github.com>
2019-11-11 11:21:03 +0530

    Added Stanford NLP Download Link
    Added Stanford NLP Download Link

--- sentiment_analysis_stanford_nlp.py
+++ sentiment_analysis_stanford_nlp.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-Navigate to your stanford NLP path and run the below command in the command prompt.
-java -mx6g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -timeout 5000
+Download Stanford NLP Library at https://stanfordnlp.github.io/CoreNLP/download.html#getting-a-copy
+Navigate to your stanford NLP path and run any of the below commands in the command prompt.
+
+java -mx6g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -timeout 50000
 java -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 9000 -timeout 15000
 
 """
@@ -11,7 +13,7 @@ from pycorenlp import StanfordCoreNLP
 nlp = StanfordCoreNLP('http://localhost:9000')
 
 text = "This movie was actually neither that funny, nor super witty. The movie was meh. I liked watching that movie. If I had a choice, I would not watch that movie again."
-text = "What amazing service Apple won't even talk to me about a question I have unless I pay them $19.95 for their stupid support!"
+
 results = nlp.annotate(text,properties={
         'annotators':'sentiment, ner, pos',
         'outputFormat': 'json',
